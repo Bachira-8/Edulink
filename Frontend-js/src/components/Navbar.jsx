83a9997c79f23a9bc20d1b0d5262d538144c0cef
Bachira-8 <dhruvvasava666@gmail.com>
2025-06-29 19:17:40 +0530

    Update Navbar.jsx

--- Frontend-js/src/components/Navbar.jsx
+++ Frontend-js/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ function Navbar() {
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-logo" onClick={() => setMenuOpen(false)}>
-          Edullink
+          Edulink
         </Link>
         <button
           className={`navbar-toggle${menuOpen ? " open" : ""}`}
